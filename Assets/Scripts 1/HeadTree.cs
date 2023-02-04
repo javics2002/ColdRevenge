fbc7d6a7f7a3155344007f0eae11e7a2e21e51dd
Lucas Andrè <lucasandreb@hotmail.com>
2023-02-04 20:47:13 +0100

    Update HeadTree.cs

--- Assets/Scripts 1/HeadTree.cs
+++ Assets/Scripts 1/HeadTree.cs
@@ -27,19 +27,65 @@ public class HeadTree : MonoBehaviour
     void Start()
     {
 
+        nodosD[nodos.HILO] = new familia { pareja = nodos.HILO1, padre = nodos.HILOS };
+        nodosD[nodos.HILO1] = new familia { pareja = nodos.HILO, padre = nodos.HILOS };
+
         nodosD[nodos.HUECO] = new familia { pareja = nodos.LLAVES, padre = nodos.HILO_USADO };
         nodosD[nodos.LLAVES] = new familia { pareja = nodos.HUECO, padre = nodos.HILO_USADO };
-        nodosD[nodos.LLAVES] = new familia { pareja = nodos.HUECO, padre = nodos.HILO_USADO };
-        nodosD[nodos.LLAVES] = new familia { pareja = nodos.HUECO, padre = nodos.HILO_USADO };
-        nodosD[nodos.LLAVES] = new familia { pareja = nodos.HUECO, padre = nodos.HILO_USADO };
-        nodosD[nodos.LLAVES] = new familia { pareja = nodos.HUECO, padre = nodos.HILO_USADO };
-        nodosD[nodos.LLAVES] = new familia { pareja = nodos.HUECO, padre = nodos.HILO_USADO };
-        nodosD[nodos.LLAVES] = new familia { pareja = nodos.HUECO, padre = nodos.HILO_USADO };
-        nodosD[nodos.LLAVES] = new familia { pareja = nodos.HUECO, padre = nodos.HILO_USADO };
-        nodosD[nodos.LLAVES] = new familia { pareja = nodos.HUECO, padre = nodos.HILO_USADO };
-        nodosD[nodos.LLAVES] = new familia { pareja = nodos.HUECO, padre = nodos.HILO_USADO };
-        nodosD[nodos.LLAVES] = new familia { pareja = nodos.HUECO, padre = nodos.HILO_USADO };
 
+        nodosD[nodos.LLAVES] = new familia { pareja = nodos.POMO, padre = nodos.PUERTA };
+        nodosD[nodos.POMO] = new familia { pareja = nodos.LLAVES, padre = nodos.PUERTA };
+
+        nodosD[nodos.HILO_USADO] = new familia { pareja = nodos.PUERTA, padre = nodos.LLAVES_USADAS };
+        nodosD[nodos.PUERTA] = new familia { pareja = nodos.HILO_USADO, padre = nodos.LLAVES_USADAS };
+
+        nodosD[nodos.HILO] = new familia { pareja = nodos.LLAVES_USADAS, padre = nodos.METODO2 };
+        nodosD[nodos.LLAVES_USADAS] = new familia { pareja = nodos.HILO, padre = nodos.METODO2 };
+
+        nodosD[nodos.MARK] = new familia { pareja = nodos.FOTO_LAGO, padre = nodos.PESCA };
+        nodosD[nodos.FOTO_LAGO] = new familia { pareja = nodos.MARK, padre = nodos.PESCA };
+
+        nodosD[nodos.WILLIAM] = new familia { pareja = nodos.PESCA, padre = nodos.HERMANO };
+        nodosD[nodos.PESCA] = new familia { pareja = nodos.WILLIAM, padre = nodos.HERMANO };
+
+        nodosD[nodos.HERMANO] = new familia { pareja = nodos.PERIODICO, padre = nodos.MOTIVO_MARK };
+        nodosD[nodos.PERIODICO] = new familia { pareja = nodos.HERMANO, padre = nodos.MOTIVO_MARK };
+
+        nodosD[nodos.METODO2] = new familia { pareja = nodos.MOTIVO_MARK, padre = nodos.FINAL2 };
+        nodosD[nodos.MOTIVO_MARK] = new familia { pareja = nodos.METODO2, padre = nodos.FINAL2 };
+
+        nodosD[nodos.FOTO_EMPRESARIAL] = new familia { pareja = nodos.MINERVA, padre = nodos.FRANK };
+        nodosD[nodos.MINERVA] = new familia { pareja = nodos.FOTO_EMPRESARIAL, padre = nodos.FRANK };
+
+        nodosD[nodos.PERIODICO] = new familia { pareja = nodos.FRANK, padre = nodos.MUERTE };
+        nodosD[nodos.FRANK] = new familia { pareja = nodos.PERIODICO, padre = nodos.MUERTE };
+
+        nodosD[nodos.MUERTE] = new familia { pareja = nodos.BANCARROTA, padre = nodos.SUICIDIO };
+        nodosD[nodos.BANCARROTA] = new familia { pareja = nodos.MUERTE, padre = nodos.SUICIDIO };
+
+        nodosD[nodos.AGUJA] = new familia { pareja = nodos.VENENO, padre = nodos.CAUSA_MUERTE };
+        nodosD[nodos.VENENO] = new familia { pareja = nodos.AGUJA, padre = nodos.CAUSA_MUERTE };
+
+        nodosD[nodos.PINCHAZO] = new familia { pareja = nodos.CAUSA_MUERTE, padre = nodos.CUELLO };
+        nodosD[nodos.CAUSA_MUERTE] = new familia { pareja = nodos.PINCHAZO, padre = nodos.CUELLO };
+
+        nodosD[nodos.CUELLO] = new familia { pareja = nodos.DORMIDO, padre = nodos.METODO };
+        nodosD[nodos.DORMIDO] = new familia { pareja = nodos.CUELLO, padre = nodos.METODO };
+
+        nodosD[nodos.LIBROS] = new familia { pareja = nodos.METODO, padre = nodos.CARA };
+        nodosD[nodos.METODO] = new familia { pareja = nodos.LIBROS, padre = nodos.CARA };
+
+        nodosD[nodos.METODO] = new familia { pareja = nodos.RADIO, padre = nodos.GRITO };
+        nodosD[nodos.RADIO] = new familia { pareja = nodos.METODO, padre = nodos.GRITO };
+
+        nodosD[nodos.CARA] = new familia { pareja = nodos.GRITO, padre = nodos.MOMENTO };
+        nodosD[nodos.GRITO] = new familia { pareja = nodos.CARA, padre = nodos.MOMENTO };
+
+        nodosD[nodos.MOMENTO] = new familia { pareja = nodos.INFORME, padre = nodos.COARTADA_MINERVA };
+        nodosD[nodos.INFORME] = new familia { pareja = nodos.MOMENTO, padre = nodos.COARTADA_MINERVA };
+
+        nodosD[nodos.COARTADA_MINERVA] = new familia { pareja = nodos.SUICIDIO, padre = nodos.FINAL };
+        nodosD[nodos.SUICIDIO] = new familia { pareja = nodos.COARTADA_MINERVA, padre = nodos.FINAL };
     }
 
     // Update is called once per frame
